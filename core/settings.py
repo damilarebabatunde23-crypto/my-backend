1e2b09e0d10ba3172dd56249e57ec7862250c849
damilarebabatunde23-crypto <damilarebabatunde23@gmail.com>
2026-05-14 03:14:22 -0700

    Update ALLOWED_HOSTS to include new domain

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ SECRET_KEY = os.environ.get(
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get(
+    'my-backend-mz1s.onrender.com',
     'ALLOWED_HOSTS',
     'localhost,127.0.0.1'
 ).split(',')
